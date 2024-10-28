5476fb23b3b2d275387b02c3cf44b4083893375b
golrokh <golrokh.fatemi15@gmail.com>
2024-10-28 18:22:52 +0100

    feat scroll lock

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
+import { disablePageScroll ,enablePageScroll } from "scroll-lock";
 
 const Header = () => {
   const pathname = useLocation();
@@ -15,17 +16,22 @@ const Header = () => {
   const toggleNavigation = () => {
     if (openNavigation) {
       setOpenNavigation(false);
+      enablePageScroll()
       
     } else {
       setOpenNavigation(true);
+      disablePageScroll()
       
     }
   };
 
   const handleClick = () => {
+    // console.log(window.location.pathname);
+    // console.log(window.location.hash);
+    
     if (!openNavigation) return;
 
-    
+    enablePageScroll()
     setOpenNavigation(false);
   };
 
@@ -35,7 +41,7 @@ const Header = () => {
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+      <div className="flex items-center px-5  lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
@@ -51,13 +57,13 @@ const Header = () => {
                 key={item.id}
                 href={item.url}
                 onClick={handleClick}
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === pathname.hash
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 
+                  ${item.onlyMobile ? "lg:hidden" : ""} 
+                  px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold 
+                  ${item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
-                    : "lg:text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                    : "lg:text-n-1/50"} 
+                  lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {item.title}
               </a>
